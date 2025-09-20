7548ab0b361c458fbd0608312b44ba0166dba4a3
bettolandro <114115590+bettolandro@users.noreply.github.com>
2025-09-20 18:18:36 -0300

    se sube el mascotas2

---
+++ mascotas2/src/main/java/com/example/mascotas2/Mascotas2Application.java
@@ -0,0 +1,13 @@
+package com.example.mascotas2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mascotas2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mascotas2Application.class, args);
+	}
+
+}
---
+++ mascotas2/src/test/java/com/example/mascotas2/Mascotas2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mascotas2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mascotas2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
